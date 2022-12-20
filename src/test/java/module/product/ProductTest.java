c8fede0c02fd195252fa55ad6f71d16bd4c49d39
anaffaria <anacarolinq.ng@gmail.com>
2022-12-20 15:01:56 -0300

    refact: added BeforeEach and changed test method names

--- src/test/java/module/product/ProductTest.java
+++ src/test/java/module/product/ProductTest.java
@@ -1,5 +1,99 @@
 package module.product;
 
-public class TestProduct {
-    
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+
+@DisplayName("Testes de API Rest do modulo de Produto")
+public class ProductTest {
+    private String token;
+
+    @BeforeEach
+    public void beforeEach(){
+        //Configurar os dados da API Rest da lojinha
+        baseURI = "http://165.227.93.41";
+        basePath = "/lojinha";
+        //Caso necessario informar a porta utilize port: 8080 por exemplo
+
+        //Obter o token do usuario admin
+        this.token = given()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "  \"usuarioLogin\": \"admin\",\n" +
+                        "  \"usuarioSenha\": \"admin\"\n" +
+                        "}")
+            .when()
+                .post("/v2/login")
+            .then()
+                .extract()
+                    .path("data.token");
+    }
+
+    @Test
+    @DisplayName("Validar que o valor do produto igual a 0.00 nao e permitido")
+    public void testValidateThatValuesLowerThanAllowedCannotBeEnteredInTheProduct(){
+
+        //Tentar inserir um produto com valor 0.00 e validar que a mensagem de erro foi apresentada e o status code retornado foi 422
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("token", this.token)
+                .body("{\n" +
+                        "  \"produtoNome\": \"Playstation 5\",\n" +
+                        "  \"produtoValor\": 0.00,\n" +
+                        "  \"produtoCores\": [\n" +
+                        "    \"preto\"\n" +
+                        "  ],\n" +
+                        "  \"produtoUrlMock\": \"\",\n" +
+                        "  \"componentes\": [\n" +
+                        "    {\n" +
+                        "      \"componenteNome\": \"Controle\",\n" +
+                        "      \"componenteQuantidade\": 1\n" +
+                        "    }\n" +
+                        "  ]\n" +
+                        "}")
+        .when()
+                .post("/v2/produtos")
+        .then()
+                .assertThat()
+                .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
+                .statusCode(422);
+    }
+
+    @Test
+    @DisplayName("Validar que o valor do produto igual a 7000.01 nao e permitido")
+    public void testValidateThatValuesGreaterThanAllowedCannotBeEnteredIntoTheProduct(){
+        //Tentar inserir um produto com valor 7.000.01 e validar que a mensagem de erro foi apresentada e o status code retornado foi 422
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("token", this.token)
+                .body("{\n" +
+                        "  \"produtoNome\": \"Playstation 5\",\n" +
+                        "  \"produtoValor\": 7000.01,\n" +
+                        "  \"produtoCores\": [\n" +
+                        "    \"preto\"\n" +
+                        "  ],\n" +
+                        "  \"produtoUrlMock\": \"\",\n" +
+                        "  \"componentes\": [\n" +
+                        "    {\n" +
+                        "      \"componenteNome\": \"Controle\",\n" +
+                        "      \"componenteQuantidade\": 1\n" +
+                        "    }\n" +
+                        "  ]\n" +
+                        "}")
+        .when()
+                .post("/v2/produtos")
+        .then()
+                .assertThat()
+                .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
+                .statusCode(422);
+    }
+
+
 }
